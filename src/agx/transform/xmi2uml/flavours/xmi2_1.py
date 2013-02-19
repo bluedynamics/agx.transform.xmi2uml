47d650de186f06ad6f23dc5c67298cfe07f59669
Philipp Auersperg-Castell <phil@bluedynamics.com>
2013-02-19 08:27:05 +0100

    be more version tolerant - go, gogo, go for regexps!

--- src/agx/transform/xmi2uml/flavours/xmi2_1.py
+++ src/agx/transform/xmi2uml/flavours/xmi2_1.py
@@ -5,6 +5,7 @@ from agx.transform.xmi2uml.interfaces import IXMIFlavour
 @implementer(IXMIFlavour)
 class XMI2_1(object):
     PROFILE = '{http://www.eclipse.org/uml2/3.0.0/UML}Profile'
+    PROFILES=[PROFILE,'{http://www.eclipse.org/uml2/4.0.0/UML}Profile']
     PACKAGED_ELEMENT = 'packagedElement'
     IMPORTED_ELEMENT = 'importedElement'
     OWNED_ATTRIBUTE = 'ownedAttribute'
@@ -13,3 +14,12 @@ class XMI2_1(object):
     OWNED_END = 'ownedEnd'
     GENERALIZATION = 'generalization'
     INTERFACE_REALIZATION = 'interfaceRealization'
+
+    @classmethod
+    def is_profile(self, tag):
+        """checks wether a tag name represents an UML profile"""
+        for prof in self.PROFILES:
+            if tag.endswith(prof):
+                return True
+        
+        return False
