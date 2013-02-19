8d9cce6b7122a03eb5d0b932b87192c61ab228b6
Philipp Auersperg-Castell <phil@bluedynamics.com>
2013-02-19 09:45:46 +0100

    comment

--- src/agx/transform/xmi2uml/flavours/xmi2_1.py
+++ src/agx/transform/xmi2uml/flavours/xmi2_1.py
@@ -5,7 +5,7 @@ from agx.transform.xmi2uml.interfaces import IXMIFlavour
 @implementer(IXMIFlavour)
 class XMI2_1(object):
     PROFILE = '{http://www.eclipse.org/uml2/3.0.0/UML}Profile'
-    PROFILES=[PROFILE,'{http://www.eclipse.org/uml2/4.0.0/UML}Profile']
+    PROFILES=[PROFILE,'{http://www.eclipse.org/uml2/4.0.0/UML}Profile'] #alternatives
     PACKAGED_ELEMENT = 'packagedElement'
     IMPORTED_ELEMENT = 'importedElement'
     OWNED_ATTRIBUTE = 'ownedAttribute'
